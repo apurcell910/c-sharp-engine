2cbd3878036c3bcb32c1db36e5781a6dd00c392b
Andrew Purcell <apurcell910@gmail.com>
2018-10-18 13:08:33 -0700

    Initial sprite stuff, still need to complete

---
+++ Source/Sprite.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Drawing;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharpSlugsEngine {
+    class Sprite {
+        private Hashtable spr = new Hashtable();
+        public Sprite() { }
+
+        /// <summary>
+        /// Add sprite to list of sprites
+        /// </summary>
+        /// <param name="key">The key value of the sprite, unique</param>
+        /// <param name="rect"></param>
+        /// <param name="color"></param>
+        /// <param name="fill"></param>
+        public void add(String key, Rectangle rect, Color color, bool fill = true) => spr.Add(key, new SpriteObj(rect, color, fill));
+        public void add(String key, int x1, int y1, int x2, int y2, Color color, bool fill = true) => spr.Add(key, new SpriteObj(x1, y1, x2, y2, color, fill));
+    }
+}
---
+++ Source/SpriteObj.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Drawing;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharpSlugsEngine {
+    class SpriteObj {
+        private Rectangle rect;
+        private Color color;
+        private bool fill;
+        private bool disp;
+
+        public SpriteObj(Rectangle rect, Color color, bool fill = true) {
+            this.rect = rect;
+            this.color = color;
+            this.fill = fill;
+        }
+
+        public SpriteObj(int x1, int y1, int x2, int y2, Color color, bool fill = true) {
+            this.rect.X = x1;
+            this.rect.Y = y1;
+            this.rect.Width = x2;
+            this.rect.Height = y2;
+            this.color = color;
+            this.fill = fill;
+        }
+
+        /// <summary>
+        /// Changes wheter or not to display the sprite
+        /// </summary>
+        /// <param name="disp">If true, display the sprite, if false, don't</param>
+        public void display(bool disp) {
+            this.disp = disp;
+        }
+    }
+}
