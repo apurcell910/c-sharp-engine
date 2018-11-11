e885643695a17af768e3351e0d64759fc7d9558f
har95337 <hsingh18@ucsc.edu>
2018-11-11 02:06:04 -0800

    Started Triangle Collider detection

---
+++ Source/Physics/EllipseCollider.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharpSlugsEngine.Physics
+{
+    class EllipseCollider : Collider
+    {
+        public override bool IsTouching(Collider other)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Source/Physics/TriangleCollider.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharpSlugsEngine.Physics
+{
+    class TriangleCollider : Collider
+    {
+        Triangle triangle;
+        public TriangleCollider(Vector2 v1, Vector2 v2, Vector2 v3) : this(new Triangle(v1, v2, v3)) { }
+
+        public TriangleCollider(Triangle tri)
+        {
+            this.triangle = tri;
+        }
+        /// <summary>
+        /// This function will check if any vertex of a triangle collider is touching this triangle collider
+        /// </summary>
+        /// <param name="other"></param>
+        /// <returns></returns>
+        /// https://stackoverflow.com/questions/2778240/detection-of-triangle-collision-in-2d-space
+        private bool checkTriangleConditions(TriangleCollider other)
+        {
+            bool inPointOne = triangle.ContainsPoint(other.triangle.VertexOne);
+            bool inPointTwo = triangle.ContainsPoint(other.triangle.VertexTwo);
+            bool inPointThree = triangle.ContainsPoint(other.triangle.VertexThree);
+
+            if (inPointOne || inPointTwo || inPointThree)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        private bool checkEllipseConditions(EllipseCollider other)
+        {
+            return true;
+        }
+
+        private bool checkRectConditions(RectangleCollider other)
+        {
+            return true;
+        }
+
+        private bool checkPolygonConditions(PolygonCollider other)
+        {
+            return true;
+        }
+
+        public override bool IsTouching(Collider other)
+        {
+            if (other is TriangleCollider triCollider)
+            {
+                return checkTriangleConditions(triCollider);
+            }
+            else if (other is PolygonCollider polyCollider)
+            {
+                return checkPolygonConditions(polyCollider);
+            }
+            else if (other is EllipseCollider ellipseCollider)
+            {
+                return checkEllipseConditions(ellipseCollider);
+            }
+            else if(other is RectangleCollider rectCollider)
+            {
+                return checkRectConditions(rectCollider);
+            }
+            return false;
+        }
+
+        
+    }
+}
