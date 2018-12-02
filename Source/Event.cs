a1e4093de579c3324bfe872e0f18fa019755a781
Timothy Su <ts63280@gmail.com>
2018-12-01 20:04:05 -0800

    Tim's documentation

--- Source/Event.cs
+++ Source/Event.cs
@@ -5,26 +5,52 @@ using System.Windows.Forms;
 
 namespace SharpSlugsEngine
 {
+    /// <summary>
+    /// Used for mouse and Keyboard events
+    /// </summary>
+    /// <warning>Not fully implemented</warning>
     public class Event
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Event"/> class.
+        /// </summary>
         public Event()
         {
         }
 
+        /// <summary>
+        /// Used for <see cref="Snippets"/>
+        /// </summary>
+        /// <param name="key">key data to broadcast</param>
+        /// <param name="Location">location data to broadcast</param>
         public delegate void Actions(Keys key, Vector2 Location);
 
+        /// <summary>
+        /// snippets of code to call in the event
+        /// </summary>
         public event Actions Snippets;
 
+        /// <summary>
+        /// Calls the current event with no data
+        /// </summary>
         public virtual void CallEvent()
         {
             Snippets?.Invoke(Keys.Clear, Vector2.Zero);
         }
 
+        /// <summary>
+        /// Calls the current event with key data
+        /// </summary>
+        /// <param name="key">key data to call with</param>
         public virtual void CallEvent(Keys key)
         {
             Snippets?.Invoke(key, Vector2.Zero);
         }
 
+        /// <summary>
+        /// Calls the current event with location data
+        /// </summary>
+        /// <param name="location">location to call with</param>
         public virtual void CallEvent(Vector2 location)
         {
             Snippets?.Invoke(Keys.Clear, location);
--- Source/Input/MouseManager.cs
+++ Source/Input/MouseManager.cs
@@ -29,81 +29,145 @@ namespace SharpSlugsEngine.Input
             HookForm();
         }
 
+        /// <summary>
+        /// Used for broadcasting location
+        /// </summary>
+        /// <param name="Location">Current mouse location</param>
         public delegate void BroadcastLocation(Vector2 Location);
 
+        /// <summary>
+        /// Used for all click events
+        /// </summary>
         public delegate void MouseClick();
 
+        /// <summary>
+        /// Broadcasts the location to events.
+        /// </summary>
         public event BroadcastLocation Broadcast;
 
+        /// <summary>
+        /// Called when left click occurs
+        /// </summary>
         public event MouseClick LeftClick
         {
             add => _leftClick += value;
             remove => _leftClick -= value;
         }
-        
+
+        /// <summary>
+        /// Called when right click occurs
+        /// </summary>
         public event MouseClick RightClick
         {
             add => _rightClick += value;
             remove => _rightClick -= value;
         }
 
+        /// <summary>
+        /// Called when middle click occurs
+        /// </summary>
         public event MouseClick MiddleClick
         {
             add => _middleClick += value;
             remove => _middleClick -= value;
         }
 
+        /// <summary>
+        /// Private backing field for <see cref="LeftClick"/>
+        /// </summary>
         private event MouseClick _leftClick;
 
+        /// <summary>
+        /// Private backing field for <see cref="RightClick"/>
+        /// </summary>
         private event MouseClick _rightClick;
 
+        /// <summary>
+        /// Private backing field for <see cref="MiddleClick"/>
+        /// </summary>
         private event MouseClick _middleClick;
 
         /// <summary>
         /// Gets current state of mouse. Contains location and which parts are clicked.
         /// </summary>
         public MouseState State { get; private set; }
-                
+
+        /// <summary>
+        /// Add an event to be called on left click
+        /// </summary>
+        /// <param name="e">event to be called</param>
         public void AddLeftClick(Event e)
         {
             LeftClick += e.CallEvent;
         }
 
+        /// <summary>
+        /// Remove an event to be called on left click
+        /// </summary>
+        /// <param name="e">event to be called</param>
         public void RemoveLeftClick(Event e)
         {
             LeftClick -= e.CallEvent;
         }
 
+        /// <summary>
+        /// Add an event to be called on right click
+        /// </summary>
+        /// <param name="e">event to be called</param>
         public void AddRightClick(Event e)
         {
             RightClick += e.CallEvent;
         }
 
+        /// <summary>
+        /// Remove an event to be called on right click
+        /// </summary>
+        /// <param name="e">event to be called</param>
         public void RemoveRightClick(Event e)
         {
             RightClick -= e.CallEvent;
         }
 
+        /// <summary>
+        /// Add an event to be called on middle click
+        /// </summary>
+        /// <param name="e">event to be called</param>
         public void AddMiddleClick(Event e)
         {
             MiddleClick += e.CallEvent;
         }
 
+        /// <summary>
+        /// Remove an event to be called on middle click
+        /// </summary>
+        /// <param name="e">event to be called</param>
         public void RemoveMiddleClick(Event e)
         {
             MiddleClick -= e.CallEvent;
         }
 
+        /// <summary>
+        /// Adds an event to get sent the location of the mouse constantly
+        /// </summary>
+        /// <param name="e">event to be called</param>
         public void AddLocationBind(Event e)
         {
             Broadcast += e.CallEvent;
         }
-            
+        
+        /// <summary>
+        /// Gets world location from camera.
+        /// </summary>
+        /// <param name="camera">sorurce camera</param>
+        /// <returns>The world coordinates of the mouse</returns>
         public Vector2 WorldLoc(Camera camera)
         {
             return camera.CameraToWorld(State.Location - camera.DrawPosition);
         }
 
+        /// <summary>
+        /// When manager is disposed of, unhooks the form.
+        /// </summary>
         void IDisposable.Dispose() => UnhookForm();
 
         /// <summary>
@@ -141,6 +205,11 @@ namespace SharpSlugsEngine.Input
             }
         }
 
+        /// <summary>
+        /// Update mousestate when mousemove triggers.
+        /// </summary>
+        /// <param name="sender">source object</param>
+        /// <param name="e">event information</param>
         private void MouseMove(object sender, MouseEventArgs e)
         {
             asyncMouseState.X = e.X;
@@ -170,6 +239,11 @@ namespace SharpSlugsEngine.Input
             gameInternal.Platform.Form.MouseMove -= new MouseEventHandler(MouseMove);
         }
 
+        /// <summary>
+        /// Update mousestate when mousedown triggers.
+        /// </summary>
+        /// <param name="sender">source object</param>
+        /// <param name="e">event information</param>
         private void MouseDown(object sender, MouseEventArgs e)
         {
             switch (e.Button)
@@ -188,6 +262,11 @@ namespace SharpSlugsEngine.Input
             }
         }
 
+        /// <summary>
+        /// Update mousestate when mouseup triggers.
+        /// </summary>
+        /// <param name="sender">source object</param>
+        /// <param name="e">event information</param>
         private void MouseUp(object sender, MouseEventArgs e)
         {
             switch (e.Button)
@@ -206,6 +285,9 @@ namespace SharpSlugsEngine.Input
             }
         }
 
+        /// <summary>
+        /// Stores status of the mouse
+        /// </summary>
         public struct MouseState
         {
             public int X;
@@ -215,6 +297,14 @@ namespace SharpSlugsEngine.Input
             public Click Center;
             public Vector2 Location;
 
+            /// <summary>
+            /// Initializes a new instance of the <see cref="MouseState"/> struct.
+            /// </summary>
+            /// <param name="x">x position of mouse</param>
+            /// <param name="y">y position of mouse</param>
+            /// <param name="l">left mouse button status</param>
+            /// <param name="r">right mouse button status</param>
+            /// <param name="c">middle mouse button status</param>
             internal MouseState(int x, int y, Click l, Click r, Click c)
             {
                 X = x;
@@ -226,22 +316,40 @@ namespace SharpSlugsEngine.Input
             }
         }
 
+        /// <summary>
+        /// Status of mouse button
+        /// </summary>
         public struct Click
         {
+            /// <summary>
+            /// Initializes a new instance of the <see cref="Click"/> struct.
+            /// </summary>
+            /// <param name="clicked">Current status of button</param>
             internal Click(bool clicked)
             {
                 IsClicked = clicked;
                 WasClicked = false;
             }
 
+            /// <summary>
+            /// Initializes a new instance of the <see cref="Click"/> struct.
+            /// </summary>
+            /// <param name="clicked">Current status of button</param>
+            /// <param name="wasClicked">Previous status of button</param>
             internal Click(bool clicked, bool wasClicked)
             {
                 IsClicked = clicked;
                 WasClicked = wasClicked;
             }
 
+            /// <summary>
+            /// Gets a value indicating whether the mouse button is currently cicked.
+            /// </summary>
             public bool IsClicked { get; private set; }
 
+            /// <summary>
+            /// Gets a value indicating whether the mouse button is immediately previously clicked.
+            /// </summary>
             public bool WasClicked { get; private set; }
         }
     }
