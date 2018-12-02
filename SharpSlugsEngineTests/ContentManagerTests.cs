8c3c790cfbca4d72c71b45d8bfb1ff24ed935822
har95337 <hsingh18@ucsc.edu>
2018-12-02 12:37:24 -0800

    Unit tests for Content Manager

---
+++ SharpSlugsEngineTests/ContentManagerTests.cs
@@ -0,0 +1,114 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SharpSlugsEngine;
+using System;
+using System.Drawing;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharpSlugsEngine.Tests
+{
+    [TestClass()]
+    public class ContentManagerTests
+    {
+        [TestMethod()]
+        public void AddImageTest()
+        {
+            TestGame game = new TestGame();
+            game.Content.AddImage("test.png", "image");
+            if (!game.Content.InManager("image")) Assert.Fail("Image was not added");
+        }
+
+        [TestMethod()]
+        public void AddFontTest()
+        {
+            TestGame game = new TestGame();
+
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetFontTest()
+        {
+            TestGame game = new TestGame();
+
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void AddSoundTest()
+        {
+            TestGame game = new TestGame();
+
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetSoundTest()
+        {
+            TestGame game = new TestGame();
+
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void ScaleImageTest()
+        {
+            TestGame game = new TestGame();
+            Size size1;
+            Size size2;
+            game.Content.AddImage("test.png", "test");
+            size1 = game.Content.GetImage("test").Size;
+
+            Bitmap bmp = game.Content.ScaleImage(game.Content.GetImage("test"), 2);
+            size2 = bmp.Size;
+            if (size1 == size2) Assert.Fail("Size was not scaled");
+        }
+
+        [TestMethod()]
+        public void GetImageTest()
+        {
+            TestGame game = new TestGame();
+            game.Content.AddImage("test.png", "test");
+
+            Bitmap bmp = game.Content.GetImage("test");
+            if (bmp == null) Assert.Fail("Bitmap is not gotten");
+        }
+
+        [TestMethod()]
+        public void InManagerTest()
+        {
+            TestGame game = new TestGame();
+            game.Content.AddImage("test.png", "test");
+            if (!game.Content.InManager("test")) Assert.Fail("Image was not found in manager");
+        }
+
+        [TestMethod()]
+        public void PrintNamesTest()
+        {
+            TestGame game = new TestGame();
+            game.Content.AddImage("test.png", "test");
+        }
+
+        [TestMethod()]
+        public void SplitImageTest()
+        {
+            TestGame game = new TestGame();
+            game.Content.SplitImage(new Bitmap("test.png"), 4, "file");
+
+            if (!game.Content.InManager("file1")) Assert.Fail("Splits didn't occur");
+        }
+
+        private class TestGame : Game
+        {
+            protected override void Draw(GameTime gameTime)
+            {
+            }
+
+            protected override void Update(GameTime gameTime)
+            {
+            }
+        }
+    }
+}
---
+++ SharpSlugsEngineTests/GraphicsManagerTests.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SharpSlugsEngine;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharpSlugsEngine.Tests
+{
+    [TestClass()]
+    public class GraphicsManagerTests
+    {
+        [TestMethod()]
+        public void ToWorldScaleTest()
+        {
+            TestGame game = new TestGame();
+            Vector2 worldScale = Vector2.Zero;
+
+            worldScale = game.Graphics.ToWorldScale(worldScale);
+            if(worldScale == Vector2.Zero) Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void ToResolutionScaleTest()
+        {
+            TestGame game = new TestGame();
+            Vector2 worldScale = Vector2.Zero;
+
+            worldScale = game.Graphics.ToResolutionScale(worldScale);
+            if (worldScale == Vector2.Zero) Assert.Fail();
+        }
+    }
+
+    class TestGame : Game
+    {
+        protected override void Draw(GameTime gameTime)
+        {
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+        }
+    }
+}
